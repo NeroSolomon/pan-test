976f86a3279397d504193c72e6977d2aa6e75f59
mingyang.tan <mingyang.tan@mobvista.com>
2020-09-22 21:04:31 +0800

    add comment detail

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -18,10 +18,33 @@ function Content() {
           >
             <img src={img} alt="内容" />
           </div>
-          <div className="item--float-left" style={{ width: "335px" }}>
+          <div
+            className="item--float-left"
+            style={{ width: "335px", overflow: "hidden" }}
+          >
             <div className={style["article__auther"] + " item--clear-float"}>
               <div className={style["article__auther-pic"]} />
               <div className={style["article__auther-name"]}>Animals Lover</div>
+            </div>
+            <div
+              className={style["article__comment"]}
+              style={{ width: "340px" }}
+            >
+              <div className={style["comment__container"]}>
+                <div className={style["comment__auther-pic"]} />
+                <div className={style["comment__auther-name"]}>
+                  Animals Lover
+                </div>
+                <div className={style["comment__detail"]}>
+                  <div className={style["comment__detail-info"]}>
+                    <span>6 天</span>
+                    <span>200 次赞</span>
+                    <span style={{ cursor: "pointer", fontWeight: "bold" }}>
+                      回复
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </Card>
