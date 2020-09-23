f5069d8469fe262f4c6dcf567b8671ae2a5cd9f3
mingyang.tan <mingyang.tan@mobvista.com>
2020-09-23 10:03:59 +0800

    add lint

--- src/components/ArticleComment.jsx
+++ src/components/ArticleComment.jsx
@@ -53,6 +53,22 @@ function ArticleComment(props) {
           </div>
         </div>
       </div>
+      <div
+        className={
+          style["comment__container"] + " " + style["item--clear-float"]
+        }
+      >
+        <div className={style["comment__auther-pic"]} />
+        <div className={style["comment__auther-name"]}>Animals Lover</div>
+        <div
+          className={
+            style["comment__like"] +
+            " " +
+            (isActive ? style["comment__like--active"] : "")
+          }
+          onClick={toggleLike}
+        ></div>
+      </div>
     </div>
   );
 }
